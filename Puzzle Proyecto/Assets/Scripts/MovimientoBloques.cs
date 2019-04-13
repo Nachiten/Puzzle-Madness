273806d5c7e89a448eba3157df7289d3a5a23472
Ignacio Baptista <ibaptista@est.frba.utn.edu.ar>
2019-04-13 14:26:04 -0300

    Movimiento Funcionando correcto

--- Puzzle Proyecto/Assets/Scripts/MovimientoBloques.cs
+++ Puzzle Proyecto/Assets/Scripts/MovimientoBloques.cs
@@ -5,15 +5,16 @@ using System;
 
 public class MovimientoBloques : MonoBehaviour
 {
-    int[,] matriz = new int[3,3] { { 1, 2, 3}, { 4, 5, 6}, { 7, 8, 0} };
+    int[,] matriz = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
     int[,] emptySlot = new int[1, 2] { { 0, 0 } };
     int fila, columna;
+    RaycastHit hit;
 
     // Update is called once per frame
     void Update()
     {
 
-        RaycastHit hit;
+        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform != null && Input.GetMouseButtonDown(0))
@@ -27,28 +28,36 @@ public class MovimientoBloques : MonoBehaviour
         }
     }
 
-    void Start() {
+    void Start()
+    {
         //mostrarMatriz(matriz);
     }
 
-    void mostrarMatriz(int[,] matriz) {
+    void mostrarMatriz(int[,] matriz)
+    {
 
-        for (int i = 0; i < 3; i++) { 
-            for (int j = 0; j < 3; j++) {
-                Debug.Log(matriz[i,j]);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Debug.Log(matriz[i, j]);
             }
         }
 
     }
 
-    void scanEmptySlot(int[,] matriz, int[,] emptySlot , int slot) {
+    void scanEmptySlot(int[,] matriz, int[,] emptySlot, int slot)
+    {
 
         // Debug.Log(slot);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
 
-                if (matriz[i, j] == slot) {
+                if (matriz[i, j] == slot)
+                {
 
                     fila = i;
                     columna = j;
@@ -62,20 +71,56 @@ public class MovimientoBloques : MonoBehaviour
 
         if (columna + 1 < 3 && matriz[fila, columna + 1] == 0)
         {
+            Transform posicion = GameObject.Find(hit.transform.gameObject.name).GetComponent<Transform>();
+
+            Vector3 posicionVector = new Vector3(posicion.position.x + 6, posicion.position.y, posicion.position.z);
+
             Debug.Log("SE MUEVE A LA DERECHA");
-            //GameObject.FindObjectOfType
+
+            posicion.position = posicionVector;
+
+            matriz[fila, columna + 1] = slot;
+            matriz[fila, columna] = 0;
         }
+
         else if (columna - 1 > -1 && matriz[fila, columna - 1] == 0)
         {
+            Transform posicion = GameObject.Find(hit.transform.gameObject.name).GetComponent<Transform>();
+
+            Vector3 posicionVector = new Vector3(posicion.position.x - 6, posicion.position.y, posicion.position.z);
+
             Debug.Log("SE MUEVE A LA IZQUIERDA");
+
+            posicion.position = posicionVector;
+
+            matriz[fila, columna - 1] = slot;
+            matriz[fila, columna] = 0;
         }
         else if (fila + 1 < 3 && matriz[fila + 1, columna] == 0)
         {
+            Transform posicion = GameObject.Find(hit.transform.gameObject.name).GetComponent<Transform>();
+
+            Vector3 posicionVector = new Vector3(posicion.position.x, posicion.position.y, posicion.position.z - 6);
+
             Debug.Log("SE MUEVE ABAJO");
+
+            posicion.position = posicionVector;
+
+            matriz[fila + 1, columna] = slot;
+            matriz[fila, columna] = 0;
         }
         else if (fila - 1 > -1 && matriz[fila - 1, columna] == 0)
         {
+            Transform posicion = GameObject.Find(hit.transform.gameObject.name).GetComponent<Transform>();
+
+            Vector3 posicionVector = new Vector3(posicion.position.x, posicion.position.y, posicion.position.z + 6);
+
             Debug.Log("SE MUEVE ARRIBA");
+
+            posicion.position = posicionVector;
+
+            matriz[fila - 1, columna] = slot;
+            matriz[fila, columna] = 0;
         }
         else
         {
