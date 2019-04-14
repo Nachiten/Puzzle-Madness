ccd2bc2053cfb3be8d085b90ac52b471c2fa13ea
Ignacio Baptista <ibaptista@est.frba.utn.edu.ar>
2019-04-14 17:44:04 -0300

    Matriz expandida a NxN
    Version: Alpha 2.0.0
    Cambios:
    - Matrices de mapas expandidas a NxN
    - Nivel 02 Terminado
    - Actualizado a version Alpha 2.0.0

--- Puzzle Madness Proyecto/Assets/Scripts/MovimientoBloques.cs
+++ Puzzle Madness Proyecto/Assets/Scripts/MovimientoBloques.cs
@@ -6,30 +6,36 @@ using UnityEngine.UI;
 
 public class MovimientoBloques : MonoBehaviour
 {
-    int[,] matriz = new int[3, 3] { { 7, 3, 5 }, { 2, 1, 0 }, { 4, 8, 6 } };
-    int[,] matrizGano = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 8, 7, 0 } };
+    public int tamañoMatriz = 3;
+
+    // Matrices para almacenar posiciones
+    int[,] matriz;
+    int[,] matrizGano;
+
     int fila, columna;
     int movimientos;
     bool gano = false;
+    bool start = false;
 
+    // Texto de movimientos
+    Text textoMovimiento;
+
+    // Cantidad de movimientos aleatorios para mezclar
     public int RandomMoves = 30;
-
-    public Text textoMovimiento;
-
+    
     /* -------------------------------------------------------------------------------- */
 
     // Se actualiza una vez por fotograma
     void Update()
     {
-        RaycastHit hit;
 
-        if (!gano)
+        if (!gano && start)
         {
             // Generar rayo para "clickear" bloque
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             // Si el rayo hace contacto con un bloque y se tiene el click IZQUIERDO persionado
-            if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform != null && Input.GetMouseButtonDown(0))
+            if (Physics.Raycast(ray, out RaycastHit hit, 100.0f) && hit.transform != null && Input.GetMouseButtonDown(0))
             {
                 // Asignar slot correcto y escanear si movimiento es posible
                 int slot = int.Parse(hit.transform.gameObject.name);
@@ -45,22 +51,30 @@ public class MovimientoBloques : MonoBehaviour
     // Al iniciar juego
     void Start()
     {
-        Debug.Log("Game Started");
-        
-        Debug.Log("Generando Movimientos Random");
+        // Inicializar Matrices
+        matriz = new int[tamañoMatriz, tamañoMatriz];
+        matrizGano = new int[tamañoMatriz, tamañoMatriz];
 
+        // Asignar Texto Movimientos
+        textoMovimiento = GameObject.Find("Numero Movimientos").GetComponent<Text>();
+
+        // Llenar matrices de datos
+        llenarMatrizes();
+
+        Debug.Log("Generando Movimientos Random");
         do // Se repite si queda en posicion ganada y hasta que haya 30 movimientos
         {
             // Generar movimiento random
-            scanEmptySlot(Random.Range(1, 9));
+            scanEmptySlot(Random.Range(1, (tamañoMatriz* tamañoMatriz)));
 
-        } while ( movimientos < RandomMoves || gano );
+        } while (movimientos < RandomMoves || gano);
 
         // Reiniviar movimientos
         movimientos = 0;
         textoMovimiento.text = "0";
 
-        Debug.Log("Movimientos aleatorios terminados");
+        Debug.Log("Movimientos aleatorios terminados | Comenzando juego...");
+        start = true;
     }
 
     /* -------------------------------------------------------------------------------- */
@@ -70,9 +84,9 @@ public class MovimientoBloques : MonoBehaviour
         gano = true;
 
         // Analizar si matrizes juego y gano son identicas
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < tamañoMatriz; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < tamañoMatriz; j++)
             {
                 if (matriz[i, j] != matrizGano[i, j])
                 {
@@ -96,9 +110,9 @@ public class MovimientoBloques : MonoBehaviour
     void scanEmptySlot(int slot)
     {
         // Analizar en que espacio estoy ahora
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < tamañoMatriz; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < tamañoMatriz; j++)
             {
                 if (matriz[i, j] == slot)
                 {
@@ -111,17 +125,17 @@ public class MovimientoBloques : MonoBehaviour
         Transform posicion = GameObject.Find(slot.ToString()).GetComponent<Transform>();
 
         // Analizar movimientos posibles de bloque
-        if (columna + 1 < 3 && matriz[fila, columna + 1] == 0)
+        if (columna + 1 < tamañoMatriz && matriz[fila, columna + 1] == 0)
         {
             // Se mueve hacia derecha
-            accion(6,0);
+            accion(6, 0);
         }
         else if (columna - 1 > -1 && matriz[fila, columna - 1] == 0)
         {
             // Se mueve hacia izquierda
             accion(-6, 0);
         }
-        else if (fila + 1 < 3 && matriz[fila + 1, columna] == 0)
+        else if (fila + 1 < tamañoMatriz && matriz[fila + 1, columna] == 0)
         {
             // Se mueve hacia abajo
             accion(0, -6);
@@ -134,8 +148,9 @@ public class MovimientoBloques : MonoBehaviour
         else { Debug.Log("No hay espacio a donde mover este bloque"); }
 
         textoMovimiento.text = movimientos.ToString();
-        
-        void accion(int offsetX, int offsetZ) {
+
+        void accion(int offsetX, int offsetZ)
+        {
 
             // Se modifica el vector posicion con la posicion correspondiente
             Vector3 posicionVector = new Vector3(posicion.position.x + offsetX, posicion.position.y, posicion.position.z + offsetZ);
@@ -153,14 +168,40 @@ public class MovimientoBloques : MonoBehaviour
     /* -------------------------------------------------------------------------------- */
 
     // Mostrar Matriz
-    void mostrarMatriz() {
-        for (int i = 0; i < 3; i++)
+    void mostrarMatrix( int[,] matrix)
+    {
+        for (int i = 0; i < tamañoMatriz; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < tamañoMatriz; j++)
             {
-                Debug.Log(matriz[i, j]);
+                Debug.Log(matrix[i, j]);
             }
         }
     }
+
+    void llenarMatrizes()
+    {
+        int contador;
+
+        contador = 1;
+
+        for (int i = 0; i < tamañoMatriz; i++)
+        {
+            for (int j = 0; j < tamañoMatriz; j++)
+            {
+                if (contador < tamañoMatriz * tamañoMatriz)
+                {
+                    matriz[i, j] = contador;
+                    matrizGano[i, j] = contador;
+                }
+                else {
+                    matriz[i, j] = 0;
+                    matrizGano[i, j] = 0;
+                }
+                contador++;
+            }
+        }
+    }
+
 
 }
